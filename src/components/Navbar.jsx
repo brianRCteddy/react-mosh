4e54931a26dc959f4de8275e295e850fc8d82b9c
Teddy <brianrcdteodoro@gmail.com>
2019-12-04 14:45:12 +0800

    Navbar from Class to Function Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 
-class Navbar extends Component {
-	render() {
-		return (
-			<div className="Navbar">
-				<nav className="navbar navbar-light bg-light">
-					<a className="navbar-brand" href="#">
-						Navbar{' '}
-						<span badge badge-pill badge-secondary>
-							{this.props.totalCounters}
-						</span>
-					</a>
-				</nav>
-			</div>
-		);
-	}
-}
+const Navbar = (props) => {
+	return (
+		<div>
+			<nav className="navbar navbar-light bg-light">
+				<a className="navbar-brand" href="#">
+					Navbar{' '}
+					<span badge badge-pill badge-secondary>
+						{props.totalCounters}
+					</span>
+				</a>
+			</nav>
+		</div>
+	);
+};
 
 export default Navbar;
